d6bfd66a9916e5bd8cfd8d46df77f634d23e7d2f
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2022-05-22 19:34:52 -0300

    feat(login-flow): setting initial flow to login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import {useState, React} from "react";
 
 import {
   Flex,
-  ScaleFade,
   Box,
+  Text,
   FormControl,
   FormLabel,
   Input,
@@ -11,55 +11,73 @@ import {
   Link,
   Button,
   Heading,
-  useColorModeValue
+  useColorModeValue,
 } from "@chakra-ui/react";
 
-import PropTypes from 'prop-types';
+const Login = ({
+    error,
+    loading,
+    onLoginClick,
+    onSignUpClick,
+    onForgotPasswordClick
+ }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-const Login = ({ onSignUpClick }) => (
-	<ScaleFade in={true} initialScale={0.9}>
-		<Flex minH={"100vh"} align={"center"} justify={"center"} bg={useColorModeValue("gray.50", "gray.800")}>
-		<Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-			<Stack align={"center"}>
-				<Heading fontSize={"2xl"}>Olá, tudo bem? bem vindo ao Wallie.</Heading>
-			</Stack>
+    return (
+        <Flex
+            minH={"100vh"}
+            align={"center"}
+            justify={"center"}
+            bg={useColorModeValue("gray.50", "gray.800")}
+        >
+            <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack align={"center"}>
+                <Heading fontSize={"4xl"}>
+                    Olá, bem vindo ao Wallie!
+                </Heading>
+            </Stack>
+            <Box
+                rounded={"lg"}
+                bg={useColorModeValue("white", "gray.700")}
+                boxShadow={"lg"}
+                p={8}
+            >
+                <Stack spacing={4}>
 
-			<Box rounded={"lg"} bg={useColorModeValue("white", "gray.700")} boxShadow={"lg"} p={8} >
-				<Stack spacing={4}>
-					<FormControl id="email">
-						<FormLabel>Email</FormLabel>
-						<Input type="email" />
-					</FormControl>
+                <FormControl id="email">
+                    <FormLabel>Email</FormLabel>
+                    <Input type="email" onChange={event => setEmail(event.target.value)}/>
+                </FormControl>
 
-					<FormControl id="password">
-						<FormLabel>Senha</FormLabel>
-						<Input type="password" />
-					</FormControl>
+                <FormControl id="password">
+                    <Stack fontSize='xs' spacing={10} direction={{ base: "row" }} align={"center"} justify={"space-between"}>
+                        <FormLabel>Senha</FormLabel>
+                        <Link color={"gray.400"} _hover={{ color:"blue.400" }}>Esqueceu a senha?</Link>
+                    </Stack>
+                    <Input type="password" onChange={event => setPassword(event.target.value)}/>
+                </FormControl>
 
-					<Stack spacing={10}>
-						<Stack direction={{ base: "column", sm: "row" }} justify={"center"}>
-							<Link color={"blue.400"} onClick={onSignUpClick}>Registre-se no Wallie.</Link>
-						</Stack>
+                <Stack spacing={10}>
+                    <Button bg={"blue.400"} color={"white"} _hover={{ bg: "blue.500" }} onClick={() => onLoginClick({ email, password })}>
+                    Entrar
+                    </Button>
+                </Stack>
 
-						<Button
-							bg={"blue.400"}
-							color={"white"}
-							_hover={{
-							bg: "blue.500"
-							}}
-						>
-							Entre
-						</Button>
-					</Stack>
-				</Stack>
-			</Box>
-		</Stack>
-		</Flex>
-	</ScaleFade>
-);
-
-Login.propTypes = {
-	onSignUpClick: PropTypes.func.isRequired
+                    <Stack fontSize='xs' direction={{ base: "column", sm: "row" }} align={"center"} justify={"center"}>
+                        <Text color={"gray.400"}>
+                            Ainda não possui uma conta?
+                        </Text>
+                        <Link color={"blue.400"} onClick={onSignUpClick}>
+                            Cadastre-se
+                        </Link>
+                    </Stack>
+                </Stack>
+            </Box>
+            </Stack>
+        </Flex>
+    );
 };
+
 
 export default Login;
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,12 +1,41 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 
 import Login from '../../components/Login';
 
+import { authenticate } from '../../services/auth.js';
+import { saveSession } from '../../services/storage';
+
 const LoginContainer = props => {
+    const [error, setError] = useState();
+	const [loading, setLoading] = useState(false);
+
 	const onSignUpClick = () => props.history.push('/sign-up');
 
+	const onForgotPasswordClick = () => props.history.push('/forgot-password');
+
+	const onLoginClick = async ({ email, password }) => {
+		setLoading(true);
+
+		const { error, data } = await authenticate({ email, password });
+
+        console.log(error, data)
+
+		setLoading(false);
+
+		if (data) {
+			saveSession(data);
+			props.history.push('/home');
+		} else {
+			setError(error.message);
+		}
+	};
+
     return <Login
-        onSignUpClick={onSignUpClick}
+            error={error}
+            loading={loading}
+            onLoginClick={onLoginClick}
+            onSignUpClick={onSignUpClick}
+            onForgotPasswordClick={onForgotPasswordClick}
     />
 };
 
